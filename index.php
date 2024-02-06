2240087d3a95428e60ec8dd1337e896074f98940
candra winata <candrawinata478@gmail.com>
2024-02-06 12:33:59 +0800

    menmabhakn

--- index.php
+++ index.php
@@ -8,5 +8,9 @@
 <body>
    <h1>hello world</h1> 
    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Doloremque excepturi vel fuga illum tenetur. Explicabo vel nesciunt minima fugiat repudiandae, corrupti dolorum necessitatibus eaque, veritatis voluptas vitae facilis rem doloribus?</p>
+
+   <li><ul>
+    <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Maiores iusto sapiente incidunt assumenda soluta? A consequuntur sunt inventore ducimus magni minima? Fugiat voluptatem dignissimos itaque amet aut minima quos veritatis.</p>
+   </ul></li>
 </body>
 </html>
